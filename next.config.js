3564c27b3905889ec6070494e7a5767a1a406544
Stancu Ionut <teraki@Stancus-iMac.local>
2025-02-12 18:10:26 +0200

    Fix: Combine next.config.js exports

--- next.config.js
+++ next.config.js
@@ -2,12 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = nextConfig;
-
-module.exports = {
   images: {
     domains: ["cdn6.f-cdn.com", "media-exp1.licdn.com", "images.pexels.com"],
   },
-}
+};
+
+module.exports = nextConfig;
