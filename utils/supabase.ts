edde1a52b7acd657775d9cbcc7500bf8eb38f7be
Stancu Ionut <fam.stancu@gmail.com>
2025-02-13 14:28:26 +0200

    fix: remove deprecated redirectTo option

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -14,7 +14,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
-    flowType: 'pkce',
-    redirectTo: siteConfig.url
+    flowType: 'pkce'
   }
 })
